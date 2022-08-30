354a5915049e585d7f0bb036d9b252aac11306a0
JohannesGaessler <johannesg@5d6.de>
2022-08-31 00:13:14 +0200

    Set YAML dump width to 10000

--- webui.py
+++ webui.py
@@ -580,7 +580,7 @@ skip_grid, sort_samples, sampler_name, ddim_eta, n_iter, batch_size, i, denoisin
             info_dict["denoising_strength"] = denoising_strength
             info_dict["resize_mode"] = resize_mode
         with open(f"{filename_i}.yaml", "w", encoding="utf8") as f:
-            yaml.dump(info_dict, f, allow_unicode=True)
+            yaml.dump(info_dict, f, allow_unicode=True, width=10000)
 
 
 def get_next_sequence_number(path, prefix=''):
