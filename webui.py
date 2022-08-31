b30afecc691125830b7c49a51c3c63335a8a0340
_nderscore <_@_._>
2022-08-31 13:42:11 -0400

    feat: add support for backslash-escaped colon character in prompts

--- webui.py
+++ webui.py
@@ -1324,18 +1324,18 @@ def img2img(prompt: str, image_editor_mode: str, init_info, mask_mode: str, mask
 
 
 prompt_parser = re.compile("""
-    (?P<prompt>                # capture group for 'prompt'
-    [^:]+                      # match one or more non ':' characters
-    )                          # end 'prompt'
-    (?:                        # non-capture group
-    :+                         # match one or more ':' characters  
-    (?P<weight>                # capture group for 'weight'
-    -?\d+(?:\.\d+)?            # match positive or negative decimal number
-    )?                         # end weight capture group, make optional 
-    \s*                        # strip spaces after weight
-    |                          # OR
-    $                          # else, if no ':' then match end of line
-    )                          # end non-capture group
+    (?P<prompt>     # capture group for 'prompt'
+    (?:\\\:|[^:])+  # match one or more non ':' characters or escaped colons '\:'
+    )               # end 'prompt'
+    (?:             # non-capture group
+    :+              # match one or more ':' characters  
+    (?P<weight>     # capture group for 'weight'
+    -?\d+(?:\.\d+)? # match positive or negative integer or decimal number
+    )?              # end weight capture group, make optional 
+    \s*             # strip spaces after weight
+    |               # OR
+    $               # else, if no ':' then match end of line
+    )               # end non-capture group
 """, re.VERBOSE)
 
 # grabs all text up to the first occurrence of ':' as sub-prompt
@@ -1343,7 +1343,7 @@ prompt_parser = re.compile("""
 # if ':' has no value defined, defaults to 1.0
 # repeats until no text remaining
 def split_weighted_subprompts(input_string, normalize=True):
-    parsed_prompts = [(match.group("prompt"), float(match.group("weight") or 1)) for match in re.finditer(prompt_parser, input_string)]
+    parsed_prompts = [(match.group("prompt").replace("\\:", ":"), float(match.group("weight") or 1)) for match in re.finditer(prompt_parser, input_string)]
     if not normalize:
         return parsed_prompts
     weight_sum = sum(map(lambda x: x[1], parsed_prompts))
