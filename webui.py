e1e1d50a94fe5286b53ddfff87e539e282fd8453
hlky <106811348+hlky@users.noreply.github.com>
2022-09-01 11:50:19 +0100

    Revert "Run model on cpu if there is only cpu (#399)"
    This reverts commit 277cc466465b76017b1f0096c50d993789f02c1b.

--- webui.py
+++ webui.py
@@ -143,10 +143,7 @@ def load_model_from_config(config, ckpt, verbose=False):
         print("unexpected keys:")
         print(u)
 
-    if device.type == "cpu":
-        model.cpu()
-    else:
-        model.cuda()
+    model.cuda()
     model.eval()
     return model
 
@@ -272,10 +269,8 @@ def create_random_tensors(shape, seeds):
     return x
 
 def torch_gc():
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-        torch.cuda.ipc_collect()
-
+    torch.cuda.empty_cache()
+    torch.cuda.ipc_collect()
 def load_LDSR():
     model_name = 'model'
     yaml_name = 'project'
@@ -290,7 +285,6 @@ def load_LDSR():
     from LDSR import LDSR
     LDSRObject = LDSR(model_path, yaml_path)
     return LDSRObject
-
 def load_GFPGAN():
     model_name = 'GFPGANv1.3'
     model_path = os.path.join(GFPGAN_dir, 'experiments/pretrained_models', model_name + '.pth')
@@ -394,10 +388,7 @@ def load_SD_model():
 
         model = instantiate_from_config(config.modelUNet)
         _, _ = model.load_state_dict(sd, strict=False)
-        if device.type == "cpu":
-            model.cpu()
-        else:
-            model.cuda()
+        model.cuda()
         model.eval()
         model.turbo = opt.optimized_turbo
 
@@ -409,7 +400,6 @@ def load_SD_model():
         modelFS = instantiate_from_config(config.modelFirstStage)
         _, _ = modelFS.load_state_dict(sd, strict=False)
         modelFS.eval()
-
 
         del sd
 
@@ -795,7 +785,7 @@ def process_images(
     output_images = []
     grid_captions = []
     stats = []
-    with torch.no_grad(), precision_scope("cuda") if torch.cuda.is_available() else precision_scope("cpu"), (model.ema_scope() if not opt.optimized else nullcontext()):
+    with torch.no_grad(), precision_scope("cuda"), (model.ema_scope() if not opt.optimized else nullcontext()):
         init_data = func_init()
         tic = time.time()
 
