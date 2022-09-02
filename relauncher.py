ec62ad14e2210e820a61f5c602b9ca636ffb2abf
Joshua Kimsey <jkimsey95@gmail.com>
2022-09-01 22:20:58 -0400

    Changed relauncher.py to have easier to set arguments for users, added inbrowser as an argument choice
    Changed `relauncher.py` to have easier to set argument parameters for users to change. No longer is editing the main `os.system`  string needed. Now, users can choose to set some of the commonly used arguments (totally open to more additions here) to be True or False, with an added area for users to enter their own custom arguments as desired.
    
    This really should make it easier for users to toggle what parameters they wish to launch with, and prevents them from needing to edit the main launch string for the `webui.py` file.
    
    Addition changes to `relauncher.py` include increasing the relaunch time from 0.5 seconds to 1 second, to allow for easier program closing from the Terminal.
    
    Changed `webui.py` to now include an  `--inbrowser` argument to open the interface automatically in a user's default browser. The default is set to `False`. Gradio params have also now been alphabetised. A notice was given to users waiting for port 7860 to open that it may take upwards of 60 seconds to become available.

--- relauncher.py
+++ relauncher.py
@@ -1,11 +1,50 @@
 import os, time
 
+# USER CHANGABLE ARGUMENTS
+
+# Change to `True` if you wish to enable these common arguments
+extra_models_cpu = False
+inbrowser = False
+optimized_turbo = False
+optimized = False
+share = False
+
+# Enter other `--arguments` you wish to use
+other_arguments = ""
+
+
+
+
+
+# BEGIN RELAUNCHER PYTHON CODE
+
+common_arguments = ""
+
+if extra_models_cpu == True:
+    common_arguments += "--extra-models-cpu "
+if optimized_turbo == True:
+    common_arguments += "--optimized-turbo "
+if optimized == True:
+    common_arguments += "--optimized "
+if share == True:
+    common_arguments += "--share "
+
+if inbrowser == True:
+    inbrowser_argument = "--inbrowser "
+else:
+    inbrowser_argument = ""
+
 n = 0
 while True:
-    print('Relauncher: Launching...')
-    if n > 0:
+    if n == 0:
+        print('Relauncher: Launching...')
+        os.system(f"python scripts/webui.py {common_arguments} {inbrowser_argument} {other_arguments}")
+        
+    else:
         print(f'\tRelaunch count: {n}')
-    os.system("python scripts/webui.py")
-    print('Relauncher: Process is ending. Relaunching in 0.5s...')
+        print('Relauncher: Launching...')
+        os.system(f"python scripts/webui.py {common_arguments} {other_arguments}")
+    
+    print('Relauncher: Process is ending. Relaunching in 1s...')
     n += 1
-    time.sleep(0.5)
+    time.sleep(1)
