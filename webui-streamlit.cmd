40faf5cee3bc8826f109bd431af6c94d82ffefd5
ZeroCool940711 <alejandrogilelias940711@gmail.com>
2022-09-06 22:10:36 -0700

    Removed some unnecessary lines from the the webui_streamlit.cmd file.

--- webui-streamlit.cmd
+++ webui-streamlit.cmd
@@ -1,19 +1,6 @@
 @echo off
 set conda_env_name=ldm
 
-# Array of model files to pre-download
-# local filename
-# local path in container (no trailing slash)
-# download URL
-# sha256sum
-MODEL_FILES=(
-    'model.ckpt /sd/models/ldm/stable-diffusion-v1 https://www.googleapis.com/storage/v1/b/aai-blog-files/o/sd-v1-4.ckpt?alt=media fe4efff1e174c627256e44ec2991ba279b3816e364b49f9be2abc0b3ff3f8556'
-    'GFPGANv1.3.pth /sd/src/gfpgan/experiments/pretrained_models https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth c953a88f2727c85c3d9ae72e2bd4846bbaf59fe6972ad94130e23e7017524a70'
-    'RealESRGAN_x4plus.pth /sd/src/realesrgan/experiments/pretrained_models https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth 4fa0d38905f75ac06eb49a7951b426670021be3018265fd191d2125df9d682f1'
-    'RealESRGAN_x4plus_anime_6B.pth /sd/src/realesrgan/experiments/pretrained_models https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth f872d837d3c90ed2e05227bed711af5671a6fd1c9f7d7e91c911a61f155e99da'
-)
-
-
 :: Put the path to conda directory after "=" sign if it's installed at non-standard path:
 set custom_conda_path=
 
