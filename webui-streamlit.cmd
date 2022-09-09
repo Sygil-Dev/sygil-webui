f62e46934b6c2c2fdf7e894d6a3a514cc6d93341
hlky <106811348+hlky@users.noreply.github.com>
2022-09-09 22:30:54 +0100

    Revert "Merge pull request #894 from Hafiidz/master"
    This reverts commit 67515f0627135f6a0d3f6f61680bc45eb01a30f6, reversing
    changes made to a05a4a9b0a38431b403faca7f154fdbd6841a068.

--- webui-streamlit.cmd
+++ webui-streamlit.cmd
@@ -2,9 +2,7 @@
 
 :: copy over the first line from environment.yaml, e.g. name: ldm, and take the second word after splitting by ":" delimiter 
 set /p first_line=< environment.yaml  
-for /f "tokens=2 delims=:" %%i in ("%first_line%") do set untrimmed_conda_env_name=%%i
-:: remove whitespaces
-for /f "tokens=* delims= " %%a in ("%untrimmed_conda_env_name%") do set conda_env_name=%%a
+for /f "tokens=2 delims=:" %%i in ("%first_line%") do set conda_env_name=%%i
 echo Environment name is set as %conda_env_name% as per environment.yaml
 
 :: Put the path to conda directory after "=" sign if it's installed at non-standard path:
