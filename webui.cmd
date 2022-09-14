1b08d867874044004138ae07840d46f1e4dc2985
Thomas Mello <work.mello@gmail.com>
2022-09-15 00:38:52 +0300

    chore: cmd readability (#1135)

--- webui.cmd
+++ webui.cmd
@@ -1,54 +1,63 @@
 @echo off
 
-set conda_env_name=ldm
+:: Run all commands using this script's directory as the working directory
+cd %~dp0
 
-:: Put the path to conda directory after "=" sign if it's installed at non-standard path:
-set custom_conda_path=
-
-IF NOT "%custom_conda_path%"=="" (
-  set paths=%custom_conda_path%;%paths%
+:: copy over the first line from environment.yaml, e.g. name: ldm, and take the second word after splitting by ":" delimiter
+for /F "tokens=2 delims=: " %%i in (environment.yaml) DO (
+  set v_conda_env_name=%%i
+  goto EOL
 )
-:: Put the path to conda directory in a file called "custom-conda-path.txt" if it's installed at non-standard path:
-FOR /F %%i IN (custom-conda-path.txt) DO set custom_conda_path=%%i
+:EOL
 
-set paths=%ProgramData%\miniconda3
-set paths=%paths%;%USERPROFILE%\miniconda3
-set paths=%paths%;%ProgramData%\anaconda3
-set paths=%paths%;%USERPROFILE%\anaconda3
+echo Environment name is set as %v_conda_env_name% as per environment.yaml
 
-for %%a in (%paths%) do (
- IF NOT "%custom_conda_path%"=="" (
-   set paths=%custom_conda_path%;%paths%
- )
+:: Put the path to conda directory in a file called "custom-conda-path.txt" if it's installed at non-standard path
+IF EXIST custom-conda-path.txt (
+  FOR /F %%i IN (custom-conda-path.txt) DO set v_custom_path=%%i
 )
 
-for %%a in (%paths%) do ( 
- if EXIST "%%a\Scripts\activate.bat" (
-    SET CONDA_PATH=%%a
+set v_paths=%ProgramData%\miniconda3
+set v_paths=%v_paths%;%USERPROFILE%\miniconda3
+set v_paths=%v_paths%;%ProgramData%\anaconda3
+set v_paths=%v_paths%;%USERPROFILE%\anaconda3
+
+for %%a in (%v_paths%) do (
+  IF NOT "%v_custom_path%"=="" (
+    set v_paths=%v_custom_path%;%v_paths%
+  )
+)
+
+for %%a in (%v_paths%) do (
+  if EXIST "%%a\Scripts\activate.bat" (
+    SET v_conda_path=%%a
     echo anaconda3/miniconda3 detected in %%a
-    goto :foundPath
- )
+    goto :CONDA_FOUND
+  )
 )
 
-IF "%CONDA_PATH%"=="" (
+IF "%v_conda_path%"=="" (
   echo anaconda3/miniconda3 not found. Install from here https://docs.conda.io/en/latest/miniconda.html
+  pause
   exit /b 1
 )
 
-call git stash
-call git pull
+:CONDA_FOUND
 
-:foundPath
-call "%CONDA_PATH%\Scripts\activate.bat"
-call conda env create -n "%conda_env_name%" -f environment.yaml
-call conda env update -n "%conda_env_name%" --file environment.yaml --prune
-call "%CONDA_PATH%\Scripts\activate.bat" "%conda_env_name%"
-python "%CD%"\scripts\relauncher.py
+if not exist "z_version_env.tmp" (
+  :: first time running, we need to update
+  set AUTO=1
+  call "update_to_latest.cmd"
+)
+
+call "%v_conda_path%\Scripts\activate.bat" "%v_conda_env_name%"
 
 :PROMPT
 set SETUPTOOLS_USE_DISTUTILS=stdlib
 IF EXIST "models\ldm\stable-diffusion-v1\model.ckpt" (
-  python scripts/relauncher.py
+  set PYTHONPATH=%~dp0
+  python scripts\relauncher.py
 ) ELSE (
-  ECHO Your model file does not exist! Place it in 'models\ldm\stable-diffusion-v1' with the name 'model.ckpt'.
+  echo Your model file does not exist! Place it in 'models\ldm\stable-diffusion-v1' with the name 'model.ckpt'.
+  pause
 )
