8e91cec4019f16af9330351377f09a013b68a216
Divided by Zer0 <mail@dbzer0.com>
2022-09-18 15:49:27 +0200

    doc: fix comment about registering

--- scripts/bridgeData_template.py
+++ scripts/bridgeData_template.py
@@ -3,7 +3,7 @@ horde_url = "https://stablehorde.net"
 # Give a cool name to your instance
 horde_name = "My Awesome Instance"
 # The api_key identifies a unique user in the horde
-# Visit https://koboldai.net/register to create one before you can join
+# Visit https://stablehorde.net/register to create one before you can join
 horde_api_key = "0000000000"
 # Put other users whose prompts you want to prioritize.
 # The owner's username is always included so you don't need to add it here, unless you want it to have lower priority than another user
