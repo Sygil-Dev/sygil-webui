745e1cc2e126fd71746c5e65523ee1bb7a7a7ba1
hlky <106811348+hlky@users.noreply.github.com>
2022-09-18 04:21:35 +0100

    Temporarily hide sd_concept_library due to missing layout()

--- scripts/webui_streamlit.py
+++ scripts/webui_streamlit.py
@@ -100,8 +100,11 @@ def layout():
                          iconName=['dashboard','model_training' ,'cloud_download', 'settings'], default_choice=0)
 		
 	if tabs =='Stable Diffusion':		
-		txt2img_tab, img2img_tab, txt2vid_tab, postprocessing_tab, concept_library_tab = st.tabs(["Text-to-Image Unified", "Image-to-Image Unified", 
-	                                                                                                "Text-to-Video","Post-Processing", "Concept Library"])
+		# txt2img_tab, img2img_tab, txt2vid_tab, postprocessing_tab, concept_library_tab = st.tabs(["Text-to-Image Unified", "Image-to-Image Unified", 
+	    #                                                                                             "Text-to-Video","Post-Processing", "Concept Library"])
+		txt2img_tab, img2img_tab, txt2vid_tab = st.tabs(
+			["Text-to-Image Unified", "Image-to-Image Unified", "Text-to-Video"]
+		)
 		#with home_tab:
 			#from home import layout
 			#layout()		
@@ -118,18 +121,18 @@ def layout():
 			from txt2vid import layout
 			layout()
 			
-		with concept_library_tab:
-			from sd_concept_library import layout
-			layout()			
+		# with concept_library_tab:
+		# 	from sd_concept_library import layout
+		# 	layout()			
 		
 	#
 	elif tabs == 'Model Manager':
 		from ModelManager import layout
 		layout()
 	
-	elif tabs == 'Textual Inversion':
-		from textual_inversion import layout
-		layout()	
+	# elif tabs == 'Textual Inversion':
+	# 	from textual_inversion import layout
+	# 	layout()	
 	
 if __name__ == '__main__':
 	layout()     
