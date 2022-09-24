3816b28d2c7f18de26a64f44f43648c487719ed2
ZeroCool940711 <alejandrogilelias940711@gmail.com>
2022-09-24 15:27:09 -0700

    Fixed error with CustomModel_available not being in session_state after the Settings page change.

--- scripts/img2img.py
+++ scripts/img2img.py
@@ -372,6 +372,7 @@ def layout():
 		with col1_img2img_layout:
 			# If we have custom models available on the "models/custom" 
 			#folder then we show a menu to select which model we want to use, otherwise we use the main model for SD
+			custom_models_available()
 			if st.session_state["CustomModel_available"]:
 				st.session_state["custom_model"] = st.selectbox("Custom Model:", st.session_state["custom_models"],
 									    index=st.session_state["custom_models"].index(st.session_state['defaults'].general.default_model),
