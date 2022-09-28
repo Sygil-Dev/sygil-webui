510d103a836676eaca4e36c9264704a72207528e
Alejandro Gil <ZeroCool940711@users.noreply.github.com>
2022-09-28 10:02:08 -0700

    Temporary fix for the jumpy UI and tabs resetting to the first one. (#1353)

--- scripts/webui_streamlit.py
+++ scripts/webui_streamlit.py
@@ -90,9 +90,9 @@ def load_css(isLocal, nameOrURL):
 
 def layout():
 	"""Layout functions to define all the streamlit layout here."""
-	#st.set_page_config(page_title="Stable Diffusion Playground", layout="wide")
-	app = st.HydraApp(title='Stable Diffusion WebUI', favicon="", sidebar_state="expanded",
-	                  hide_streamlit_markers=False, allow_url_nav=True , clear_cross_app_sessions=False)
+	st.set_page_config(page_title="Stable Diffusion Playground", layout="wide")
+	#app = st.HydraApp(title='Stable Diffusion WebUI', favicon="", sidebar_state="expanded",
+	                  #hide_streamlit_markers=False, allow_url_nav=True , clear_cross_app_sessions=False)
 
 	with st.empty():
 		# load css as an external file, function has an option to local or remote url. Potential use when running from cloud infra that might not have access to local path.
