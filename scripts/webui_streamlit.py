27ded7ce52c760b204fe7bb367a0239e431f1bd9
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-10-05 22:52:38 +0530

    Update webui_streamlit.py

--- scripts/webui_streamlit.py
+++ scripts/webui_streamlit.py
@@ -16,7 +16,9 @@
 
 # base webui import and utils.
 #import streamlit as st
-
+import torch,os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+device = torch.device("cuda:0")
 # We import hydralit like this to replace the previous stuff
 # we had with native streamlit as it lets ur replace things 1:1
 #import hydralit as st 
