c2f1970e6be51cb7c439a0b91c7fc9aa7ee09a47
hlky <106811348+hlky@users.noreply.github.com>
2022-10-06 02:40:36 +0100

    convert pt embed to bin

--- scripts/sd_utils.py
+++ scripts/sd_utils.py
@@ -1562,9 +1562,17 @@ def load_learned_embed_in_clip(learned_embeds_path, text_encoder, tokenizer, tok
 
     # separate token and the embeds
     if learned_embeds_path.endswith('.pt'):
-        print(loaded_learned_embeds['string_to_token'])
-        trained_token = list(loaded_learned_embeds['string_to_token'].keys())[0]
-        embeds = list(loaded_learned_embeds['string_to_param'].values())[0]
+        # old format
+        # token = * so replace with file directory name when converting
+        trained_token = os.path.basename(learned_embeds_path)
+        params_dict = {
+            trained_token: torch.tensor(list(loaded_learned_embeds['string_to_param'].items())[0][1])
+        }
+        learned_embeds_path = os.path.splitext(learned_embeds_path)[0] + '.bin'
+        torch.save(params_dict, learned_embeds_path)
+        loaded_learned_embeds = torch.load(learned_embeds_path, map_location="cpu")
+        trained_token = list(loaded_learned_embeds.keys())[0]
+        embeds = loaded_learned_embeds[trained_token]
         
     elif learned_embeds_path.endswith('.bin'):
         trained_token = list(loaded_learned_embeds.keys())[0]
