cd99e6d57da6fc109d75889894fc35f897c550b5
cmdr2 <secondary.cmdr2@gmail.com>
2022-10-26 16:38:32 +0530

    Add Library\usr\bin to the PATH

--- webui.cmd
+++ webui.cmd
@@ -32,7 +32,7 @@ IF EXIST custom-conda-path.txt (
 )
 
 set INSTALL_ENV_DIR=%cd%\installer_files\env
-set PATH=%INSTALL_ENV_DIR%;%INSTALL_ENV_DIR%\Library\bin;%INSTALL_ENV_DIR%\Scripts;%PATH%
+set PATH=%INSTALL_ENV_DIR%;%INSTALL_ENV_DIR%\Library\bin;%INSTALL_ENV_DIR%\Scripts;%INSTALL_ENV_DIR%\Library\usr\bin;%PATH%
 
 set v_paths=%INSTALL_ENV_DIR%
 set v_paths=%v_paths%;%ProgramData%\miniconda3
